88d40d98bea35f281eab9a1358e09ec5f2193c07
Gurucharan rao <54024246+Raogurucharan@users.noreply.github.com>
2022-04-22 18:35:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,33 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
-const spreadsheetId = "1L8ALAmG11nCy5pHUcu05NkM_eLSvWCLpjV_vSp7dg1o";
+//const spreadsheetId = "1L8ALAmG11nCy5pHUcu05NkM_eLSvWCLpjV_vSp7dg1o";
 
+app.get("/spreadsheet/:spreadsheetId", async (req, res) => {
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "Keys.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+  const spreadsheetId = req.params.spreadsheetId;
+
+  // Create client instance for auth
+  const client = await auth.getClient();
+
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  //const spreadsheetId = "1L8ALAmG11nCy5pHUcu05NkM_eLSvWCLpjV_vSp7dg1o";
+
+  const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Sheet1",
+  });
+
+  res.send(getRows.data);
+
+});
 
 app.post("/", async (req, res) => {
   const { request, name } = req.body;
@@ -26,19 +51,6 @@ app.post("/", async (req, res) => {
   const googleSheets = google.sheets({ version: "v4", auth: client });
 
   const spreadsheetId = "1L8ALAmG11nCy5pHUcu05NkM_eLSvWCLpjV_vSp7dg1o";
-
-  // Get metadata about spreadsheet
-  const metaData = await googleSheets.spreadsheets.get({
-    auth,
-    spreadsheetId,
-  });
-
-  // Read rows from spreadsheet
-  const getRows = await googleSheets.spreadsheets.values.get({
-    auth,
-    spreadsheetId,
-    range: "Sheet1",
-  });
 
   // inserting values
   await googleSheets.spreadsheets.values.append({
